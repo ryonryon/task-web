47b0726958580eb318c3d95964991f4fdbd3b47f
ryotogashi <ryo.togashi.ca@gmail.com>
2019-06-05 14:58:33 -0700

    2

---
+++ task_management/views/index_view.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.views.generic import TemplateView
+from ..models.question import Question
+
+
+class IndexView(TemplateView):
+    template_name = "task_management/index.html"
+
+    def get(self, request, *args, **kwargs):
+        latest_question_list = Question.objects.order_by('-pub_date')[:5]
+        template = loader.get_template('task_management/index.html')
+        context = {
+            'latest_question_list': latest_question_list,
+        }
+        return HttpResponse(template.render(context, request))
+
+
