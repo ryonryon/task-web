f91f24b4df68479fd2986c95cfa0b7d18acb01fc
ryotogashi <ryo.togashi.ca@gmail.com>
2019-06-05 15:11:52 -0700

    delete comment

--- task_management/urls.py
+++ task_management/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-# from .. import task_management
 from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:question_id>/', views.DetailView.as_view(), name='detail'),
-    # path('<int:question_id>/results/', views.results, name='results'),
-    # path('<int:question_id>/votes/', views.vote, name='vote'),
 ]
