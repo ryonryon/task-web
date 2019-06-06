f0ea03a9e61b2dfd6fa654661636a1ecb66c98bd
ryotogashi <ryo.togashi.ca@gmail.com>
2019-06-06 11:14:51 -0700

    2

---
+++ task_management/models/task.py
@@ -0,0 +1,10 @@
+
+class Task:
+
+    def __init__(self, user_id, user, title, detail, publish_date, is_finished):
+        self.user_id = user_id
+        self.user = user
+        self.title = title
+        self.detail = detail
+        self.publish_date = publish_date
+        self.is_finished = is_finished
