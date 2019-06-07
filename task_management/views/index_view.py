84405a6e40f5c876fc02696e13c1b02f5e48c2d8
ryotogashi <ryo.togashi.ca@gmail.com>
2019-06-07 08:56:32 -0700

    delete task_list due to that architecture has memory problem.

--- task_management/views/index_view.py
+++ task_management/views/index_view.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.views.generic import TemplateView
-from task_management.models.task_list import TaskList
+from task_management.models.task_model import TaskModel
+from task_management.models.task import Task
 
 
 class IndexView(TemplateView):
@@ -10,12 +11,34 @@ class IndexView(TemplateView):
     def get(self, request, *args, **kwargs):
         template = loader.get_template(self.template_name)
 
-        task_finished_list = TaskList(True)
-        task_remain_list = TaskList(False)
+        task_finished_list = []
+        task_remain_list = []
+
+        for task_model in TaskModel.objects.order_by('id'):
+            if task_model.is_finished is True:
+                task = Task(
+                    task_id=task_model.id
+                    , user=task_model.user
+                    , title=task_model.title
+                    , detail=task_model.detail
+                    , publish_date=task_model.publish_date
+                    , is_finished=task_model.is_finished
+                )
+                task_finished_list.append(task)
+            else:
+                task = Task(
+                    task_id=task_model.id
+                    , user=task_model.user
+                    , title=task_model.title
+                    , detail=task_model.detail
+                    , publish_date=task_model.publish_date
+                    , is_finished=task_model.is_finished
+                )
+                task_remain_list.append(task)
 
         context = {
-            'task_list': task_finished_list.get_task_list,
-            'task_finished_list': task_remain_list.get_task_list,
+            'task_list': task_finished_list,
+            'task_finished_list': task_remain_list,
         }
-        return HttpResponse(template.render(context, request))
 
+        return HttpResponse(template.render(context, request))
