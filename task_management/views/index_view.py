9422d825f64de63cc5eb907660cdb3a688dcb4ed
ryotogashi <ryo.togashi.ca@gmail.com>
2019-06-08 20:11:56 -0700

    delete comment

--- task_management/views/index_view.py
+++ task_management/views/index_view.py
@@ -12,31 +12,6 @@ class IndexView(TemplateView):
     def get(self, request, *args, **kwargs):
         template = loader.get_template(self.template_name)
 
-        # task_finished_list = []
-        # task_remain_list = []
-        #
-        # for task_model in TaskModel.objects.order_by('id'):
-        #     if task_model.is_finished is True:
-        #         task = Task(
-        #             task_id=task_model.id
-        #             , user=task_model.user
-        #             , title=task_model.title
-        #             , detail=task_model.detail
-        #             , publish_date=task_model.publish_date
-        #             , is_finished=task_model.is_finished
-        #         )
-        #         task_finished_list.append(task)
-        #     else:
-        #         task = Task(
-        #             task_id=task_model.id
-        #             , user=task_model.user
-        #             , title=task_model.title
-        #             , detail=task_model.detail
-        #             , publish_date=task_model.publish_date
-        #             , is_finished=task_model.is_finished
-        #         )
-        #         task_remain_list.append(task)
-
         context = self.get_status_lists(TaskModel.objects.order_by('id'))
 
         return HttpResponse(template.render(context, request))
